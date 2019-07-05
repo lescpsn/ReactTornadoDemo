d369eacae35898946b4ee888808b6b7daaff6ff4
lescpsn <lescpsn@aliyun.com>
2019-07-05 13:57:36 +0800

    (*) 优化工程

---
+++ back_end/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+################################################################################
+def main_test():
+    print("Enter Function main:")
+
+if __name__ == "__main__":
+    main()
