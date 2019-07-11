f718e2486a34a307619e6b5f40e1ceb3045fa0b6
lescpsn <lescpsn@aliyun.com>
2019-07-11 23:46:39 +0800

    (*) 优化工程

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'antd';
-
+import { Button, Divider } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-
-      <Button type="primary">Primary</Button>
-      <Button>Default</Button>
-      <Button type="dashed">Dashed</Button>
-      <Button type="danger">Danger</Button>
-      <Button type="link">Link</Button>
-    </div>
+    <div>dfdfdfd</div>
   );
+
 }
 
 export default App;
