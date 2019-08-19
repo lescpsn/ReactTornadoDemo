715747566b6d35459b9dd5f1ed76463048447a0d
lescpsn <lescpsn@aliyun.com>
2019-08-19 21:44:20 +0800

    (*) 优化工程

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Button from 'antd/es/button';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Button type="primary">Button</Button>
       </div>
-    );
+    )
   }
 }
 
