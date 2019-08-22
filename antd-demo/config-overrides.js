3005be949cf4d379f483e2ee90c051fc78deff74
lescpsn <lescpsn@aliyun.com>
2019-08-22 08:38:38 +0800

    (*) 优化工程

--- antd-demo/config-overrides.js
+++ antd-demo/config-overrides.js
@@ -1,19 +1,15 @@
-// module.exports = function override(config, env) {
-//     // do stuff with the webpack config...
-//     return config;
-//   };
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
-
-const { override, fixBabelImports } = require('customize-cra');
-
-//   - module.exports = function override(config, env) {
-//   -   // do stuff with the webpack config...
-//   -   return config;
-//   - };
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css',
+    // style: 'css',
+    style: true,
+  }),
+
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },  
   }),
 );  
--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Button } from 'antd';
 import './App.css';
 
-
 class App extends Component {
   render() {
     return (
