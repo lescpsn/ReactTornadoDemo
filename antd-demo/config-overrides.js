694e6cf2c7f3ee69e11d489a51f101b401632ed8
lescpsn <lescpsn@aliyun.com>
2019-09-11 14:44:28 +0800

    (*) 优化工程

--- antd-demo/config-overrides.js
+++ antd-demo/config-overrides.js
@@ -1,4 +1,14 @@
-module.exports = function override(config, env) {
-    // do stuff with the webpack config...
-    return config;
-  };
+// module.exports = function override(config, env) {
+//     // do stuff with the webpack config...
+//     return config;
+// };
+
+const { override, fixBabelImports } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+);
